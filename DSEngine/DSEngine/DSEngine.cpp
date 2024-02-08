dabd2682e4a1063106308970d612a890c2889044
Ethan <ethan.cutter@hotmail.co.uk>
2024-02-08 20:46:14 +0000

    Added Entity Manager for the start of the Entity Component System.

--- DSEngine/DSEngine/DSEngine.cpp
+++ DSEngine/DSEngine/DSEngine.cpp
@@ -3,6 +3,7 @@
 GameCamera Camera;
 
 float E_DeltaTime = 0.0f;
+float lastFrameTime = 0.0f;
 
 void DSEngine::Run()
 {
@@ -13,41 +14,7 @@ void DSEngine::Run()
 
     Camera.TranslateView(glm::vec3(0.0f, 0.0f, -3.0f));
     Camera.SetProjection(glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f));
-    float lastFrameTime = 0.0f;
 
-    BasicCubeNode lightNode;
-    lightNode.m_Shader = &lightShader;
-
-    GameObject lightCube;
-    lightCube.AddNode(&lightNode);
-    lightCube.Translate(-2.0f, 2.0f, -6.0f);
-
-    GameObject basicCube;
-
-    basicCube.Translate(0.0f, 2.0f, -5.0f);
-
-    // Todo: Try to find a way so the Vertex Arrays aren't repeated for the same vertices for the same type of node.
-    BasicCubeNode node = BasicCubeNode();
-
-    node.m_Shader = &cubeShader;
-
-    basicCube.AddNode(&node);
-
-    GameObject basicCube2;
-    basicCube.Rotate(45.0f, glm::vec3(10.0f, 0.0f, 0.0f));
-    basicCube2.Translate(0.0f, 0.0f, -5.0f);
-    basicCube2.AddNode(&node);
-    basicCube2.Parent = &basicCube;
-
-
-    GameObject floor;
-    floor.Scale(glm::vec3(100.0f, 1.0f, 100.0f));
-    floor.Translate(0.0, -2.0f, 0.0f);
-    floor.AddNode(&node);
-
-
-    GameObject pivotPoint;
-    lightCube.Parent = &pivotPoint;
 
     // render loop
     // -----------
@@ -56,11 +23,6 @@ void DSEngine::Run()
         float currentFrameTime = glfwGetTime();
         E_DeltaTime = currentFrameTime - lastFrameTime;
         lastFrameTime = currentFrameTime;
-
-        cubeShader.Use();
-        cubeShader.SetVec3("objectColor", 0.1f, 0.4f, 1.0f);
-        cubeShader.SetVec3("lightColor", 1.0f, 1.0f, 1.0f);
-        cubeShader.SetVec3("lightPos", lightCube.Position());
 
         // input
         // -----
@@ -71,24 +33,7 @@ void DSEngine::Run()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        cubeShader.Use();
-        cubeShader.SetVec3("objectColor", 0.1f, 0.4f, 1.0f);
-        cubeShader.SetVec3("lightColor", 1.0f, 1.0f, 1.0f);
-        cubeShader.SetVec3("lightPos", lightCube.Position());
-        cubeShader.SetVec3("viewPos", Camera.Position);
-
         Camera.Update();
-        glBindVertexArray(node.m_VAO);
-        basicCube.Update(&Camera);
-        basicCube2.Update(&Camera);
-        lightCube.Update(&Camera);
-
-
-        floor.Update(&Camera);
-
-        //pivotPoint.Rotate(20.0f * deltaTime, glm::vec3(45.0f, 45.0f, 0.0f));
-
-        // glBindVertexArray(0); // no need to unbind it every time 
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
